750db7d57f7946afc70f84f22e77f356d2377f14
Clwn <148951899+Clwning@users.noreply.github.com>
2023-10-25 10:48:44 +0200

    FRRRRR
    Cleaned up code, simplified date stuff, and improved file handling

--- proxify.py
+++ proxify.py
@@ -1,9 +1,15 @@
-import os, time, concurrent.futures, threading, random, string, json, sys, re, pathlib
-import requests, colorama, pystyle, datetime, asyncio, tls_client
-from pystyle import Write, System, Colors, Colorate, Anime
-from colorama import Fore, Style
+import os
+import time
+import concurrent.futures
+import threading
+import re
+import pathlib
+import requests
 from datetime import datetime
+from colorama import Fore, Style
+from pystyle import Write, System, Colors, Colorate, Anime
 
+# color codes
 red = Fore.RED
 yellow = Fore.YELLOW
 green = Fore.GREEN
@@ -22,12 +28,8 @@ output_lock = threading.Lock()
 start_time = time.time()
 
 def get_time_rn():
-    date = datetime.now()
-    hour = date.hour
-    minute = date.minute
-    second = date.second
-    timee = "{:02d}:{:02d}:{:02d}".format(hour, minute, second)
-    return timee
+    now = datetime.now()
+    return now.strftime("%H:%M:%S")
 
 def ui():
     System.Clear()
@@ -61,62 +63,48 @@ REGEX = re.compile(
 with open("sources.txt") as f:
     links = f.read().splitlines()
 
-with open(f"proxies.txt", "w") as f:
-    f.write("")
-
-
 scraped = 0
 duplicate = 0
 proxies = []
 
 def scrape_proxy_links(link):
-    global proxies
     global scraped
     link_proxies = []
     response = requests.get(link)
     if response.status_code == 200:
-
         with output_lock:
-            sys.stdout.flush()
             time_rn = get_time_rn()
             print(f"[ {pink}{time_rn}{reset} ] | ( {lightblue}CHECKED{reset} ) Site " + link[:40] + f"********{reset}\n", end='')
-
-            html = response.text # Get Page HTML
-            if tuple(REGEX.finditer(html)): # Check if theres regex matches
-                for proxy in tuple(REGEX.finditer(html)): # for each result in there
-                    link_proxies.append(proxy.group(1)) # Append it to our list
+            html = response.text
+            if tuple(REGEX.finditer(html)):
+                for proxy in tuple(REGEX.finditer(html)):
+                    link_proxies.append(proxy.group(1))
                 time_rn = get_time_rn()
-                print(f"[ {pink}{time_rn}{reset} ] | ( {cyan} FOUND {reset} ) {pretty}{len(link_proxies)}{reset} Proxies\n", end='') # Found X Proxies
+                print(f"[ {pink}{time_rn}{reset} ] | ( {cyan} FOUND {reset} ) {pretty}{len(link_proxies)}{reset} Proxies\n", end='')
             else:
                 time_rn = get_time_rn()
-                print(f"[ {pink}{time_rn}{reset} ] | ( {red}FAILED{reset} ) No proxies at {link}{reset}\n", end='') # No Proxies
-        scraped += len(link_proxies) # Add to total number
-
-        for proxy in link_proxies: # Handle proxies in memory
+                print(f"[ {pink}{time_rn}{reset} ] | ( {red}FAILED{reset} ) No proxies at {link}{reset}\n", end='')
+        scraped += len(link_proxies)
+        for proxy in link_proxies:
             proxies.append(proxy)
-        time_rn = get_time_rn()
-    return
 
 def cleanup():
     global duplicate
-    proxy_seen = set() # holds lines already seen
-    outfile = open("proxies.txt", "w")
-    for proxy in proxies:
-        if proxy not in proxy_seen: # not a duplicate
-            outfile.write(proxy + "\n")
-            proxy_seen.add(proxy)
-        else:
-            duplicate += 1
-    outfile.close()
-
+    proxy_seen = set()
+    with open("proxies.txt", "w") as outfile:
+        for proxy in proxies:
+            if proxy not in proxy_seen:
+                outfile.write(proxy + "\n")
+                proxy_seen.add(proxy)
+            else:
+                duplicate += 1
     time_rn = get_time_rn()
     print(f"[ {pink}{time_rn}{reset} ] | ( {blue}REMOVED{reset} ) {pretty}{duplicate} {reset}duplicates\n", end='')
 
 def main():
-    num_threads = len(pathlib.Path("sources.txt").read_text())
+    num_threads = len(links)
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         executor.map(scrape_proxy_links, links)
-
     time_rn = get_time_rn()
     print(f"\n[ {pink}{time_rn}{reset} ] | ( {blue}CHECKED{reset} ) {pretty}{len(links)} {reset}Sites\n", end='')
     print(f"[ {pink}{time_rn}{reset} ] | ( {blue}SCRAPED{reset} ) {pretty}{scraped} {reset}potential Proxies\n", end='')
@@ -124,4 +112,5 @@ def main():
     time_rn = get_time_rn()
     print(f"[ {pink}{time_rn}{reset} ] | ( {green}WRITTEN{reset} ) {pretty}{scraped - duplicate}{reset} Proxies in {round(time.time() - start_time, 2)} seconds\n", end='')
     print(f"\n[ {pink}{time_rn}{reset} ] | ( {green}EXITING{reset} ) {pink}GLHF{reset}\n\n", end='')
+
 main()
